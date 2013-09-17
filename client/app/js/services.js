3d70cf28b39478d796797c289916735e9aa532f7
shock_one <shockone89@gmail.com>
2013-09-17 22:22:33 +0300

    Rewrite scrollfix from jQuery to Angular directive

--- client/app/js/services.js
+++ client/app/js/services.js
@@ -84,40 +84,23 @@ angular.module('contactManager.services', []).
 			}
 		}
 	})
-	.factory("flash", function($rootScope) {
-		var queue = [], currentMessage = {};
-
-		$rootScope.$on('$routeChangeSuccess', function () {
-			if (queue.length > 0)
-				currentMessage = queue.shift();
-			else
-				currentMessage = {};
-		});
-
+	.directive('scrollFix', ['$window', function ($window) {
 		return {
-			set: function (message) {
-				var msg = message;
-				queue.push(msg);
+			link: function (scope, elm, attrs) {
+				var viewPort = angular.element($window);
+				var element = angular.element(elm);
 
-			},
-			get: function (message) {
-				return currentMessage;
-			},
-			pop: function (message) {
-				switch (message.type) {
-					case 'success':
-						toastr.success(message.body, message.title);
-						break;
-					case 'info':
-						toastr.info(message.body, message.title);
-						break;
-					case 'warning':
-						toastr.warning(message.body, message.title);
-						break;
-					case 'error':
-						toastr.error(message.body, message.title);
-						break;
-				}
+				viewPort.on('scroll', function() {
+					if ( viewPort.height() >= element.height()){
+						var topOffset = Math.max(0,77-viewPort.scrollTop());
+
+						element.css('position', 'fixed');
+						element.css('right', 0);
+						element.css('top', topOffset);
+					} else {
+						element.css('position', 'static');
+					}
+				});
 			}
 		};
-	});
+	}]);
